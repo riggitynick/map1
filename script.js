7771c5c54dd71feca71e7d3b65d1df086a53c713
Nick Root <40615687+riggitynick@users.noreply.github.com>
2026-07-19 15:37:57 -0500

    first upload

---
+++ script.js
@@ -0,0 +1,42 @@
+
+AFRAME.registerComponent('rotation-reader', {
+
+    tick: (function () {
+      var position = new THREE.Vector3();
+      var quaternion = new THREE.Quaternion();
+  
+      return function () {
+        this.el.object3D.getWorldPosition(position);
+        this.el.object3D.getWorldQuaternion(quaternion);
+        // position and rotation now contain vector and quaternion in world space.
+      };
+    })
+  });
+
+
+ AFRAME.registerComponent("healthandwellness", {
+      init: function() {
+       this.el.addEventListener("click", () => {  
+      //  window.open("http://www.google.com", "new win");     - this opens a new window w/ no popup
+       document.getElementById("hwPopup").style.display ="unset";
+          });}});
+function removeHwPopup(){
+  document.getElementById("hwPopup").style.display = "none";
+
+};
+
+             AFRAME.registerComponent('vanish', {
+    // ...
+    pause: function () {
+      
+      this.removeEventListener();
+    }
+    // ...
+  });
+   
+
+     
+        window.onload = () => {navigator.geolocation.getCurrentPosition((position) => {
+          document.querySelector('a-shere').setAttribute('gps-entity-place', `latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude};`)
+        });
+       }
