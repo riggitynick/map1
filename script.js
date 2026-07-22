2dde2445d136d56bddb58a551688d9c35706e282
Nick Root <40615687+riggitynick@users.noreply.github.com>
2026-07-21 20:25:58 -0500

    Refactor script.js to handle GPS position updates
    Replaced the existing rotation-reader component with a new implementation that adds a box entity at the GPS position. Removed commented-out code and added functionality to handle GPS updates.

--- script.js
+++ script.js
@@ -1,58 +1,25 @@
+window.onload = () => {
+    let testEntityAdded = false;
 
-import * as LocAR from 'locar';
-import * as THREE from 'three';
+    const el = document.querySelector("[gps-new-camera]");
 
-
-AFRAME.registerComponent('rotation-reader', {
-
-    tick: (function () {
-      var position = new THREE.Vector3();
-      var quaternion = new THREE.Quaternion();
-  
-      return function () {
-        this.el.object3D.getWorldPosition(position);
-        this.el.object3D.getWorldQuaternion(quaternion);
-        // position and rotation now contain vector and quaternion in world space.
-      };
-    })
-  });
-
-//   AFRAME.registerComponent('query-selector-example', {
-//   init: function () {
-//     this.entities = document.querySelectorAll('.a-entity');
-//   },
-  
-//   tick: function () {
-//     // Don't call query selector in here, query beforehand.
-//     for (let i = 0; i < this.entities.length; i++) {
-//       // Do something with entities.
-//     }
-//   }
-// });
-
- AFRAME.registerComponent("healthandwellness", {
-      init: function() {
-       this.el.addEventListener("click", () => {  
-      //  window.open("http://www.google.com", "new win");     - this opens a new window w/ no popup
-       document.getElementById("hwPopup").style.display ="unset";
-          });}});
-function removeHwPopup(){
-  document.getElementById("hwPopup").style.display = "none";
-
+    el.addEventListener("gps-camera-update-position", e => {
+        if(!testEntityAdded) {
+            alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
+            // Add a box to the north of the initial GPS position
+            const entity = document.createElement("a-box");
+            entity.setAttribute("scale", {
+                x: 20, 
+                y: 20,
+                z: 20
+            });
+            entity.setAttribute('material', { color: 'red' } );
+            entity.setAttribute('gps-new-entity-place', {
+                latitude: e.detail.position.latitude + 0.001,
+                longitude: e.detail.position.longitude
+            });
+            document.querySelector("a-scene").appendChild(entity);
+        }
+        testEntityAdded = true;
+    });
 };
-
-  //            AFRAME.registerComponent('vanish', {
-  //   // ...
-  //   pause: function () {
-      
-  //     this.removeEventListener();
-  //   }
-  //   // ...
-  // });
-   
-
-     
-        window.onload = () => {navigator.geolocation.getCurrentPosition((position) => {
-          document.querySelector('a-asset-item').setAttribute('gps-entity-place', `latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude};`)
-        });
-       }
